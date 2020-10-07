131db4113ee112fe57b9f857721912801c896416
Shreshth Bhatt <shreshthgit56@gmail.com>
2020-10-08 00:26:28 +0530

    final

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -6,7 +6,7 @@
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.12;
+pragma solidity ^0.5.0;
 
 contract SimpleBank {
 
